78ee7713190b4d00e8b3f59983c4db5f7232599e
antonymundanmany <antonymundanmany@gmail.com>
2024-02-10 12:34:05 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
+import Addpost from './components/Addpost';
+import Viewpost from './components/Viewpost';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
 
                 <Route path='/' element={<Login/>} />
                 <Route path='/register' element={<Register/>} />
+                <Route path='/addpost' element={<Addpost/>} />
+                <Route path='/viewpost' element={<Viewpost/>} />
 
             </Routes>
 
